d9bcc9630409ce8ccfc0402f2334535370d54823
d4rkb1ue <7874364@gmail.com>
2017-02-10 15:18:24 +0800

    finish Permutation

--- week-2-stack-queue/dequesAndRandomizedQueues/Permutation.java
+++ week-2-stack-queue/dequesAndRandomizedQueues/Permutation.java
@@ -11,9 +11,27 @@ G
 A
 ---
 */
-
+import edu.princeton.cs.algs4.*;
+import java.util.NoSuchElementException;
 public class Permutation {
 	public static void main(String[] args){
 		RandomizedQueue<String> q = new RandomizedQueue<String>();
-	}
+		int outCount = Integer.parseInt(args[0]);
+		int inCount = 0;
+		// StdOut.println(outCount);
+		while (!StdIn.isEmpty()){
+			String item = StdIn.readString();
+			if (item != null && item.length() > 0){
+				q.enqueue(item);
+				inCount++;
+			}
+		}// end read file
+		if (inCount < outCount){
+			throw new NoSuchElementException("incoming element < need to be print out");
+		}
+		while(outCount>0){
+			StdOut.println(q.dequeue());
+			outCount--;
+		}
+	}	
 }
