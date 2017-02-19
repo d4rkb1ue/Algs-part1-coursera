6ba71d58a9f5855212bec7185eefb1aee2894a6e
d4rkb1ue <7874364@gmail.com>
2017-02-19 21:01:26 +0800

    clean the Fast.java

--- week-3-mergesort/patternRecognition/FastCollinearPoints.java
+++ week-3-mergesort/patternRecognition/FastCollinearPoints.java
@@ -15,27 +15,27 @@ import java.util.*;
 import edu.princeton.cs.algs4.*;
 import java.lang.NullPointerException;
 
-
-
-
-
-
-
-
-// --------------------------------------------------TODO: 如果一条线上有 5 个点，第一次循环找到了这个线；从下一个点开始，又有一个 4 个点的同样的线，怎么排除？
-
-
-
-
-
-
-
 public class FastCollinearPoints {
 
 	// a line segment contains minium of 4 points
 	private static final int LINE_LENGTH = 4;
 	private LineSegment[] segments;
-	private Point[] points;
+
+	private class LineSegmentWithPoints{
+		private LineSegment line;
+		private Point[] points;
+		public LineSegmentWithPoints(LineSegment line, Point[] points){
+			this.line = line;
+			this.points = points;
+		}
+		public LineSegment getLine(){
+			return line;
+		}
+		public Point[] getPoints(){
+			return points;
+		}
+	}
+	
 
     // finds all line segments containing 4 or more points
 	public FastCollinearPoints(Point[] points){
@@ -44,9 +44,8 @@ public class FastCollinearPoints {
 
 		int N = points.length;
 		if (N < LINE_LENGTH) segments = new LineSegment[0];
-		this.points = points;
 
-		edu.princeton.cs.algs4.Stack<LineSegment> tmpSegments = new edu.princeton.cs.algs4.Stack<LineSegment>();
+		edu.princeton.cs.algs4.Stack<LineSegmentWithPoints> tmpSegments = new edu.princeton.cs.algs4.Stack<LineSegmentWithPoints>();
 
 		/**
 		 * if points[p1]->points[p2] is not on a line px->py,
@@ -60,23 +59,17 @@ public class FastCollinearPoints {
 			 * sort directly other points
 			 * it's not important how other points sorted
 			 */
-			for (int kk = p+1; kk < N; kk++){
-				p("-" + points[p].toString() + " slopeTo " + points[kk].toString() + " = " + points[p].slopeTo(points[kk]));
-			}
-			p("----after sort----");
-			Arrays.sort(points, p+1, N-1, points[p].slopeOrder());
-			for (int kk = p+1; kk < N; kk++){
-				p("-" + points[p].toString() + " slopeTo " + points[kk].toString() + " = " + points[p].slopeTo(points[kk]));
-			}
+
+			Arrays.sort(points, p+1, N, points[p].slopeOrder());
 			int start = p;
 			while(start <= N-LINE_LENGTH){
-				start = pushNextLineSegment(points, start, tmpSegments);				
+				start = pushNextLineSegment(points, p, start, tmpSegments, this);
 			}
 		}
 
 		segments = new LineSegment[tmpSegments.size()];
 		for (int i = 0; tmpSegments.size() > 0; i++){
-			segments[i] = tmpSegments.pop();
+			segments[i] = tmpSegments.pop().getLine();
 		}
 	}
 
@@ -93,41 +86,48 @@ public class FastCollinearPoints {
 	 * @param  stack    push new line into this
 	 * @return          end point of this segment + 1
 	 */
-	private static int pushNextLineSegment(Point[] points, int start, edu.princeton.cs.algs4.Stack<LineSegment> stack){
-		p("call pushNextLineSegment");
-		Point p = points[start];
+	private static int pushNextLineSegment(Point[] points, int base, int start, edu.princeton.cs.algs4.Stack<LineSegmentWithPoints> stack, FastCollinearPoints thisFast){
+		Point p = points[base];
 		// current line segment total length
 		int currentLength = LINE_LENGTH;
 		for (int i = start+1; i+LINE_LENGTH-2 < points.length; i++){
 			// if i.slopeTo(p).equals(i+LINE_LENGTH-2)
 			// then p->this->(this+1)->(this+2) is a line (when LINE_LENGTH == 4)
 			// and this line length == LINE_LENGTH
-			
-			p("for i = " + i);
 
 			if (p.slopeTo(points[i]) == p.slopeTo(points[i+LINE_LENGTH-2])){
-				p(p.toString() + " -> " + points[i].toString() + " is a line");
 
-				// if (lineIsInTheStack(p, points[i], stack)) continue;
+				if (lineIsInTheStack(p, points[i], stack)) continue;
 
 				// explore whether this line.length > LINE_LENGTH
 				while (i+currentLength-1 < points.length && p.slopeTo(points[i]) == p.slopeTo(points[i+currentLength-1])) currentLength++;
 				
-				Point minPoint = getMaxOrMinPoint(p, points, i, i+currentLength-2, false);
-				Point maxPoint = getMaxOrMinPoint(p, points, i, i+currentLength-2, true);
-				p("getMaxOrMinPoint from " + minPoint.toString() + " to " + maxPoint.toString());
+				Point[] pointsOnLine = new Point[currentLength];
+				copyToPoints(points, p, pointsOnLine, i, i+currentLength-2);
+
+				Point minPoint = getMaxOrMinPoint(pointsOnLine, false);
+				Point maxPoint = getMaxOrMinPoint(pointsOnLine, true);
+
+				stack.push(thisFast.new LineSegmentWithPoints(new LineSegment(minPoint, maxPoint), pointsOnLine));
 				
-				stack.push(new LineSegment(maxPoint, minPoint));
-				
-				return i+currentLength-1;
+				return base+currentLength-1;
 			}// end if find a line
 		}// end for
 		return points.length;
 	}
+
+	private static void copyToPoints(Point[] source, Point plusOne, Point[] des, int start, int end){
+		des[0] = plusOne;
+		int d = 1;
+		for (int i = start; i <= end; i++, d++){
+			des[d] = source[i];
+		}
+	}
+
 	/**
-	 * check if line a-> b inside the stack
+	 * check if line a->b inside the stack
 	 */
-	private boolean lineIsInTheStack(Point a, Point b, edu.princeton.cs.algs4.Stack<LineSegment> stack){
+	private static boolean lineIsInTheStack(Point a, Point b, edu.princeton.cs.algs4.Stack<LineSegmentWithPoints> stack){
 		/**
 		 * 这么遍历，在最差情况下，得耗时多久？N^2
 		 * A:
@@ -136,49 +136,26 @@ public class FastCollinearPoints {
 		 * Consider the n points of the form: (x, y) for x = 0, 1, 2, and 3 and y = 0, 1, 2, ..., n / 4. 
 		 * This means that if you store all of the (maximal) collinear sets of points, you will need quadratic space in the worst case.
 		 */
-		for (LineSegment line : stack){
-			if (isSameLine(a, b, line)){
-				return true;
-			}
+		Point[] points;
+		for (LineSegmentWithPoints lwp : stack){
+			points = lwp.getPoints();
+			Arrays.sort(points);
+			if (Arrays.binarySearch(points, a) > -1 && Arrays.binarySearch(points, b) > -1) return true;
 		}
 		return false;
 	}
-	/**
-	 * check if a and b both on this line
-	 * if so, namely a->b is this line 
-	 */
-	private static boolean isSameLine(Point a, Point b, LineSegment line){
-		if (onTheLine(a, line) && onTheLine(b, line)) return true;
-		return false;
-	}
-	/**
-	 * return if point p is on the line
-	 */
-	private static boolean onTheLine(Point p, LineSegment line){
-		// line.toString
-		// TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		return false;
-	}
+
 
 	/**
-	 * version 2.0 
-	 * with start and end
-	 * [points[start], points[end]]: boundary included
-	 *
-	 * @param  p 	  addtional to points
-	 * @param  points source
-	 * @param  start  start at
-	 * @param  end    end with (include points[end])
-	 * @param  max    if true, get the max point out of all points; else, get the min point
-	 * @return        if max, return max; else min
+	 * @param max : if true, get the max point out of all points; else, get the min point
+	 * @return Point: if max, return max; else min
 	 */
-	private static Point getMaxOrMinPoint(Point p, Point[] points, int start, int end, boolean max){
-		if (points == null || points.length == 0 || start > end || points.length < end) 
-			throw new NullPointerException("no point.");
-		Point ret = p;
-		for (int i = start; i <= end; i++){
-			if (max && points[i].compareTo(ret) > 0) ret = points[i];
-			else if (!max && points[i].compareTo(ret) < 0) ret = points[i];
+	private static Point getMaxOrMinPoint(Point[] points, boolean max){
+		if (points == null || points.length == 0) throw new NullPointerException("no point.");
+		Point ret = points[0];
+		for (Point p : points){
+			if (max && p.compareTo(ret) > 0) ret = p;
+			else if (!max && p.compareTo(ret) < 0) ret = p;
 		}
 		return ret;
 	}
@@ -210,6 +187,7 @@ public class FastCollinearPoints {
     }
 
     public static void main(String[] args) {
+    	// LineSegmentWithPoints lwp = new LineSegmentWithPoints(new LineSegment(new Point(1,2), new Point(3,4)), null);
 
     	// read the n points from a file
     	In in = new In(args[0]);
@@ -224,26 +202,27 @@ public class FastCollinearPoints {
 		// if(pointsHaveRepeated(points)) throw new java.lang.IllegalArgumentException("point repeat.");
 
     	// draw the points
-    	StdDraw.enableDoubleBuffering();
-    	StdDraw.setXscale(0, 32768);
-    	StdDraw.setYscale(0, 32768);
-    	for (Point p : points) {
-    		p.draw();
-    	}
-    	StdDraw.show();
+    	// StdDraw.enableDoubleBuffering();
+    	// StdDraw.setXscale(0, 32768);
+    	// StdDraw.setYscale(0, 32768);
+    	// for (Point p : points) {
+    	// 	p.draw();
+    	// }
+    	// StdDraw.show();
 
     	// print and draw the line segments
+    	
+    	Stopwatch time = new Stopwatch();
     	FastCollinearPoints collinear = new FastCollinearPoints(points);
-    	p("---- numberOfSegments = " + collinear.numberOfSegments());
+    	StdOut.println("time = " + time.elapsedTime());
     	for (LineSegment segment : collinear.segments()) {
     		StdOut.println(segment);
-    		segment.draw();
+    		// segment.draw();
     	}
-    	StdDraw.show();
+    	// StdDraw.show();
     }
 
-    private static void p(String s){
-    	StdOut.println(s);
-    }
-
+    // private static void p(String s){
+    	// StdOut.println(s);
+    // }
 }
